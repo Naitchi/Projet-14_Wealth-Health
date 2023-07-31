ef2dfc8b65e960eed1aec776c7227908626ed508
Naitchi <assasindu39@gmail.com>
2023-07-31 15:44:15 +0200

    ⚡️ Opimazed employee page

--- src/pages/employees.js
+++ src/pages/employees.js
@@ -1,12 +1,14 @@
 import Head from 'next/head';
 import React from 'react';
-import styles from '../styles/Employees.module.css';
+import Link from 'next/link';
 
 import EmployeesArray from '@/components/EmployeesArray/EmployeesArray';
 
+import styles from '../styles/Employees.module.css';
+
 export default function Employees() {
   return (
-    <>
+    <React.Fragment>
       <Head>
         <title>HRnet - Current Employees</title>
         <meta name="description" content="HRnet - Current Employees" />
@@ -18,8 +20,8 @@ export default function Employees() {
           <h1>Current Employees</h1>
         </div>
         <EmployeesArray />
-        <a href="/">Home</a>
+        <Link href="/">Home</Link>
       </main>
-    </>
+    </React.Fragment>
   );
 }
